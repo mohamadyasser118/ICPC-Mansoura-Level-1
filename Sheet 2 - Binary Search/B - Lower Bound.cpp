fa9dd0d0d133f95fc5f70be26387d2dd2d34e1cb
Mohamad Yasser <mohamadyasser118@gmail.com>
2024-06-24 07:12:38 +0300

    Add files via upload

---
+++ Sheet 2 - Binary Search/B - Lower Bound.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int lowerBound(const vector<int>& v, int target) {
+    int l = 0, r = v.size() - 1;
+    int result = -1;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (v[mid] >= target) {
+            result = mid + 1;
+            r = mid-1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return result;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> v(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    while (Q--) {
+        int X;
+        cin >> X;
+        int result = lowerBound(v, X);
+        cout << result << endl;
+    }
+
+    return 0;
+}
